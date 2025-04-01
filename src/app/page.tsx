fe0ada04da51d0f85a76fbaaac9bf03c55b643d3
Majid-Arab <arabmajid12@gmail.com>
2025-04-01 16:20:21 +0500

    basic structure of hero secction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,33 @@
-import { ColorSchemesSwitcher } from "@/components/color-schemes-switcher";
-import {
-  AppShell,
-  AppShellHeader,
-  AppShellMain,
-  Group,
-  Text,
-  Title,
-} from "@mantine/core";
+import { AppShell, AppShellMain, Button, Group, Title } from "@mantine/core";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <AppShell header={{ height: 60 }} padding="md">
-      <AppShellHeader>
-        <Group className="h-full px-md">
-          <Image
-            className="dark:invert"
-            src="https://nextjs.org/icons/next.svg"
-            alt="logo"
-            width={100}
-            height={100}
-          />
-        </Group>
-      </AppShellHeader>
-      <AppShellMain>
-        <Title className="text-center mt-20">
-          Welcome to{" "}
-          <Text
-            inherit
-            variant="gradient"
-            component="span"
-            gradient={{ from: "pink", to: "yellow" }}
-          >
-            Mantine
-          </Text>{" "}
-          +
-          <Text
-            inherit
-            variant="gradient"
-            component="span"
-            gradient={{ from: "blue", to: "green" }}
-          >
-            TailwindCSS
-          </Text>
-        </Title>
-        <Text
-          className="text-center text-gray-700 dark:text-gray-300 max-w-[500px] mx-auto mt-xl"
-          ta="center"
-          size="lg"
-          maw={580}
-          mx="auto"
-          mt="xl"
-        >
-          This starter Next.js project includes a minimal setup for Mantine with
-          TailwindCSS. To get started edit page.tsx file.
-        </Text>
+    <AppShell>
+      <AppShellMain className="flex flex-col items-center justify-center">
+        <Image
+          src="/assets/logo.png"
+          alt="Logo"
+          width={500}
+          height={500}
+          className="w-[50px] h-[60px]"
+          objectFit="cover"
+        />
+        {/* // Animated Light Bar that'll stretch on hover */}
 
-        <div className="flex justify-center mt-10">
-          <ColorSchemesSwitcher />
-        </div>
+        {/* Button  */}
+        <Button variant="light" color="#fff" size="md" radius="xl" className="">
+          Leading Marketing Agency
+        </Button>
+        <Title className="text-[300px] uppercase">
+          Faarns
+          <Group className="text-sm" justify="space-between">
+            <span>Website</span>
+            <span>Marketing</span>
+            <span>Saas</span>
+            <span>SEO</span>
+          </Group>
+        </Title>
       </AppShellMain>
     </AppShell>
   );
