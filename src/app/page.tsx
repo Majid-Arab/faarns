5c27b69e65e1b327f7e1234d2a0d59e14745a1c9
Majid-Arab <arabmajid12@gmail.com>
2025-04-02 16:20:54 +0500

    moved hero section to new file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,11 @@
-import { Box, Button, Group } from "@mantine/core";
-import Image from "next/image";
+import Contact from "@/sections/Contac";
+import Hero from "@/sections/Hero";
 
 export default function Home() {
   return (
-    <Box className="flex flex-col items-center justify-center h-screen">
-      <Box className="flex flex-col items-center justify-center gap-2">
-        <Image
-          src="/assets/logo.png"
-          alt="Logo"
-          width={500}
-          height={500}
-          className="w-[50px] h-[60px]"
-          objectFit="cover"
-        />
-        {/* // Animated Light Bar that'll stretch on hover */}
-        <div className="w-20 bg-black border-solid border-white border-2"></div>
-        {/* Button  */}
-        <Button
-          variant="light"
-          color="#fff"
-          size="md"
-          radius="xl"
-          className="px-3 tracking-wide"
-        >
-          Leading Marketing Agency
-        </Button>
-      </Box>
-      <Box className="leading-none">
-        <h1 className="text-[300px] font-bold uppercase m-0 p-0">Faarns</h1>
-        <Group className="text-lg font-bold -mt-2" justify="space-between">
-          <span>Website</span>
-          <span>Marketing</span>
-          <span>Saas</span>
-          <span>SEO</span>
-        </Group>
-      </Box>
-    </Box>
+    <>
+      <Hero />
+      <Contact />
+    </>
   );
 }
---
+++ src/sections/Hero.tsx
@@ -0,0 +1,44 @@
+import { Box, Button, Group } from "@mantine/core";
+import Image from "next/image";
+
+import React from "react";
+
+const Hero = () => {
+  return (
+    <Box className="flex flex-col items-center justify-center h-screen">
+      <Box className="flex flex-col items-center justify-center gap-2">
+        <Image
+          src="/assets/logo.png"
+          alt="Logo"
+          width={500}
+          height={500}
+          className="w-[50px] h-[60px]"
+          objectFit="cover"
+        />
+        {/* // Animated Light Bar that'll stretch on hover */}
+        <div className="w-20 bg-black border-solid border-white border-2"></div>
+        {/* Button  */}
+        <Button
+          variant="light"
+          color="#fff"
+          size="md"
+          radius="xl"
+          className="px-3 tracking-wide"
+        >
+          Leading Marketing Agency
+        </Button>
+      </Box>
+      <Box className="leading-none">
+        <h1 className="text-[300px] font-bold uppercase m-0 p-0">Faarns</h1>
+        <Group className="text-lg font-bold -mt-2" justify="space-between">
+          <span>Website</span>
+          <span>Marketing</span>
+          <span>Saas</span>
+          <span>SEO</span>
+        </Group>
+      </Box>
+    </Box>
+  );
+};
+
+export default Hero;
