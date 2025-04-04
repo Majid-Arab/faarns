d134522abd133a82a17c1a0231ac2e625dc3cbe6
Abdul Majid <arabmajid12@gmail.com>
2025-04-04 09:49:44 +0500

    type error fixe

--- src/components/floating-navbar.tsx
+++ src/components/floating-navbar.tsx
@@ -7,48 +7,39 @@ import {
 import Link from "next/link";
 import { cn } from "../../lib/utils";
 import { Button } from "@mantine/core";
-import { IconPhoto } from '@tabler/icons-react';
+import { IconPhoto } from "@tabler/icons-react";
 
+type NavItem = {
+  link: string;
+  icon?: React.ReactNode;
+  name: string;
+};
 
-export const FloatingNav = ({
-  navItems,
-  className,
-}: {
-  navItems: {
-    name: string;
-    link: string;
-    icon?: JSX.Element;
-  }[];
+type FloatingNavProps = {
+  navItems: NavItem[];
   className?: string;
-}) => {
+};
 
+export const FloatingNav = ({ navItems, className }: FloatingNavProps) => {
   const [visible] = useState(true);
 
   return (
     <AnimatePresence mode="wait">
       <motion.div
-        initial={{
-          opacity: 1,
-          y: -100,
-        }}
-        animate={{
-          y: visible ? 0 : -100,
-          opacity: visible ? 1 : 0,
-        }}
-        transition={{
-          duration: 0.2,
-        }}
+        initial={{ opacity: 1, y: -100 }}
+        animate={{ y: visible ? 0 : -100, opacity: visible ? 1 : 0 }}
+        transition={{ duration: 0.2 }}
         className={cn(
-          "flex max-w-fit  fixed bottom-2 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4",
-          className,
+          "flex max-w-fit fixed bottom-2 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2 items-center justify-center space-x-4",
+          className
         )}
       >
-        {navItems.map((navItem: any, idx: number) => (
+        {navItems.map((navItem) => (
           <Link
-            key={`link=${idx}`}
+            key={`link-${navItem.link}`}
             href={navItem.link}
             className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500",
+              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
@@ -57,9 +48,10 @@ export const FloatingNav = ({
         ))}
         <Button
           leftSection={<IconPhoto size={14} />}
-          className="border inset-shadow-sm inset-shadow-indigo-500 text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
+          className="border inset-shadow-sm inset-shadow-indigo-500 text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full"
+        >
           <span>Got a concept?</span>
-          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
+          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px" />
         </Button>
       </motion.div>
     </AnimatePresence>
