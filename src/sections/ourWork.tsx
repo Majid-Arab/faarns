383f31f1a19fa15ee9031e52b8b9119fc20cbf3a
Abdul Majid <arabmajid12@gmail.com>
2025-04-05 17:27:47 +0500

    80% responsiveness of our work section

--- src/sections/ourWork.tsx
+++ src/sections/ourWork.tsx
@@ -5,29 +5,29 @@ import React from 'react'
 
 const OurWork = () => {
     return (
-        <div className='h-full py-8 bg-white rounded-2xl flex justify-between items-center gap-2 overflow-hidden'>
+        <div className='h-80 py-8 bg-white rounded-2xl flex justify-center relative'>
             {/* Images  */}
-            <div className="flex flex-col">
-                <Image src="/assets/image.png" alt='Dots' height={500} width={500} className=' h-20 rounded-md -rotate-6 -mb-4 -ml-1' />
-                <Image src="/assets/image.png" alt='Dots' height={500} width={500} className=' h-20 rounded-md -rotate-6 -mb-4 -ml-3' />
-                <Image src="/assets/image.png" alt='Dots' height={500} width={500} className=' h-20 rounded-md -rotate-6 -mb-4 -ml-6' />
+            <div className="flex flex-col gap-20 md:gap-2 absolute right-20">
+                <Image src="/assets/image.png" alt='Dots' height={500} width={500} className='max-w-[500px] h-20 rounded-md   ' />
+                <Image src="/assets/image.png" alt='Dots' height={500} width={500} className='max-w-[500px] h-20 rounded-md   ' />
+                <Image src="/assets/image.png" alt='Dots' height={500} width={500} className='max-w-[500px] h-20 rounded-md   ' />
 
             </div>
             {/* Centered Content  */}
             <div className='bg-white rounded-2xl flex flex-col justify-center items-center gap-2'>
                 <Image src="/assets/dots.png" alt='Dots' height={500} width={500} className='w-3' />
-                <Title className='text-black text-5xl'>Some of our works</Title>
-                <Text size="sm" className="text-black max-w-[500px] text-center text-[20px]">
+                <Title className='text-black text-3xl md:text-5xl text-center'>Some of our works</Title>
+                <Text size="sm" className="text-black max-w-[350px] md:max-w-[500px] text-center text-[16px] md:text-[20px]">
                     Each project in our collection reflects the passion and creativity we bring to every digital endeavor. Fro sleek web design to dynamic branding solutions,our portfolio highlights the diversity of our expertise
                 </Text>
                 <Button variant='outline' color='black' className='rounded-md' size='xl' rightSection={<IconArrowRight size={25} />}>Explore our works</Button>
 
             </div>
             {/* Images  */}
-            <div className="flex flex-col">
-                <Image src="/assets/image.png" alt='Dots' height={500} width={500} className=' h-20 rounded-md rotate-6 -mb-4 -mr-1' />
-                <Image src="/assets/image.png" alt='Dots' height={500} width={500} className=' h-20 rounded-md rotate-6 -mb-4 -mr-5' />
-                <Image src="/assets/image.png" alt='Dots' height={500} width={500} className=' h-20 rounded-md rotate-6 -mb-4 -mr-6' />
+            <div className="flex flex-col gap-20 md:gap-2 absolute left-20">
+                <Image src="/assets/image.png" alt='Dots' height={500} width={500} className='max-w-[500px] h-20 rounded-md   ' />
+                <Image src="/assets/image.png" alt='Dots' height={500} width={500} className='max-w-[500px] h-20 rounded-md   ' />
+                <Image src="/assets/image.png" alt='Dots' height={500} width={500} className='max-w-[500px] h-20 rounded-md   ' />
             </div>
         </div>
     )
