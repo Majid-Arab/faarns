7d1053a156ef87927246dd3e4f4af692a02002a6
Majid-Arab <arabmajid12@gmail.com>
2025-04-07 06:46:00 +0500

    why us section, width and height setup and also title setup

--- src/sections/WhyUs.tsx
+++ src/sections/WhyUs.tsx
@@ -4,10 +4,12 @@ import React from 'react'
 
 const WhyUs = () => {
     return (
-        <div className='bg-[url(/assets/whyus.png)] bg-cover'>
-            <Button variant='default'>Why Choose Us</Button>
-            <Title><span className="italic">Why</span> FAARNS is The <span className="italic">Right Choice</span> For You</Title>
-            <LeadGrid />
+        <div className='bg-[url(/assets/whyus.png)] w-full bg-cover flex justify-center py-10'>
+            <div className='w-[1400px]'>
+                <Button variant='default' radius="lg">Why Choose Us</Button>
+                <Title className='my-2  text-3xl md:text-5xl '><span className="italic">Why</span> FAARNS is The <span className="italic">Right Choice</span> For You</Title>
+                <LeadGrid />
+            </div>
         </div>
     )
 }
