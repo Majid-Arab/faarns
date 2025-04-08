8bfda18114e88eade6d1a201b9b4a11bc5f091db
Majid-Arab <arabmajid12@gmail.com>
2025-04-08 05:12:03 +0500

    grid card Icon setting

--- src/components/GridCard.tsx
+++ src/components/GridCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Group, Paper, Text, ThemeIcon } from '@mantine/core';
+import { Paper, Text } from '@mantine/core';
 
 type Prop = {
     icon: React.ElementType;
@@ -9,14 +9,10 @@ type Prop = {
 export function GridCard({ icon: Icon, title, description }: Prop) {
     return (
         <Paper withBorder className="px-[25px] py-[15px] rounded-3xl text-black bg-[linear-gradient(308.04deg,_#F0F4F9_4.87%,_#B3C3D8_100%)]">
-            <ThemeIcon
-                size="xl"
-                radius="xl"
-                variant="outline"
-                color='black'
-            >
-                <Icon size={50} stroke={2} />
-            </ThemeIcon>
+            <div className="inline-block p-[8px] rounded-full borer-solid border-black border-2">
+
+                <Icon size={50} stroke={2} className="" />
+            </div>
             <Text className='text-4xl' fw={900} mt="md">
                 {title}
             </Text>
