f806f19f64544c7fee0a9553065fd94f90493db4
Majid-Arab <arabmajid12@gmail.com>
2025-04-08 06:14:32 +0500

    grid cards responsive

--- src/components/GridCard.tsx
+++ src/components/GridCard.tsx
@@ -1,24 +1,26 @@
-import { Paper, Text } from '@mantine/core';
+import { Paper, Text } from "@mantine/core";
 
 type Prop = {
-    icon: React.ElementType;
-    title: string;
-    description: string;
-}
+  icon: React.ElementType;
+  title: string;
+  description: string;
+};
 
 export function GridCard({ icon: Icon, title, description }: Prop) {
-    return (
-        <Paper withBorder className="px-[25px] py-[15px] rounded-3xl text-black bg-[linear-gradient(308.04deg,_#F0F4F9_4.87%,_#B3C3D8_100%)]">
-            <div className="inline-block p-[8px] rounded-full borer-solid border-black border-2">
-
-                <Icon size={50} stroke={2} className="" />
-            </div>
-            <Text className='text-4xl' fw={900} mt="md">
-                {title}
-            </Text>
-            <Text size="" mt="sm">
-                {description}
-            </Text>
-        </Paper>
-    );
+  return (
+    <Paper
+      withBorder
+      className="px-[25px] py-[15px] rounded-3xl text-black bg-[linear-gradient(308.04deg,_#F0F4F9_4.87%,_#B3C3D8_100%)]"
+    >
+      <div className="inline-block p-[8px] rounded-full borer-solid border-black border-2">
+        <Icon stroke={2} className="size-2 md:size-3" />
+      </div>
+      <Text className="text-2xl md:text-4xl" fw={900} mt="md">
+        {title}
+      </Text>
+      <Text className="text-sm md:text-md" mt="sm">
+        {description}
+      </Text>
+    </Paper>
+  );
 }
