c0b33edac35c79c8f33f4a24dd2a8626a9ad0c84
Majid-Arab <arabmajid12@gmail.com>
2025-04-08 22:42:23 +0500

    gri section button and bg image fixes

--- src/sections/WhyUs.tsx
+++ src/sections/WhyUs.tsx
@@ -1,25 +1,17 @@
 import { LeadGrid } from "@/components/grid";
 import ItalicText from "@/components/ItlaicText";
-import { Button, Title } from "@mantine/core";
+import { Title } from "@mantine/core";
 import React from "react";
 
 const WhyUs = () => {
   return (
-    <div className="bg-[url(/assets/whyus.png)] w-full bg-cover flex justify-center px-2 py-10">
+    <div className="bg-[url(/assets/wh.png)] w-full bg-cover bg-no-repeat bg-center flex justify-center px-2 py-10">
       <div className="w-[1400px]">
-        <button className="border-1 border-white text-md md:text-xl rounded-full bg-[rgba(255, 255, 255, 0.15)] text-white px-1">
-          <Button
-            variant="light"
-            color="white"
-            radius="lg"
-            className="border-1 border-white"
-          >
-            Why Choose Us
-          </Button>{" "}
+        <button className="border-1 border-white text-md md:text-xl rounded-full bg-[rgba(255, 255, 255, 0.15)] bg-light text-white px-[25px] py-[5px]">
           Why Choose Us
         </button>
-        <Title className="my-2 sm:text-center text-3xl md:text-5xl ">
-          <ItalicText text="Why" /> FAARNS is The{" "}
+        <Title className="my-2 text-center md:text-start text-3xl md:text-5xl ">
+          <ItalicText text="Why" /> FAARNS is The
           <ItalicText text="Right Choice" /> For You
         </Title>
         <LeadGrid />
