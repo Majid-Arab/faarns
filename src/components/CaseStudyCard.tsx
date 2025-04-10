1301334347dccc1056ac3a226fffb6e4f1abc076
Majid-Arab <arabmajid12@gmail.com>
2025-04-10 22:08:10 +0500

    case study responsive

--- src/components/CaseStudyCard.tsx
+++ src/components/CaseStudyCard.tsx
@@ -10,25 +10,45 @@ export function CaseStudyCard() {
       </Group>
 
       <Group>
-        <Badge variant="outline" color="gray" size="xl" radius="md">
+        <Badge
+          variant="outline"
+          color="gray"
+          size="xl"
+          radius="md"
+          className="text-sm md:text-xl"
+        >
           12 days left
         </Badge>
-        <Badge variant="outline" color="gray" size="xl" radius="md">
+        <Badge
+          variant="outline"
+          color="gray"
+          size="xl"
+          radius="md"
+          className="text-sm md:text-xl"
+        >
           12 days left
         </Badge>
-        <Badge variant="outline" color="gray" size="xl" radius="md">
+        <Badge
+          variant="outline"
+          color="gray"
+          size="xl"
+          radius="md"
+          className="text-sm md:text-xl"
+        >
           12 days left
         </Badge>
       </Group>
-      <Title className="text-cyan-950" fw={500}>
+      <Title className="text-cyan-950 text-2xl md:text-4xl" fw={500}>
         5.3 minor release (September 2022)
       </Title>
-      <Text className="text-[25px] text-cyan-950">
+      <Text className="text-lg md:text-[25px] text-cyan-950">
         Form context management, Switch, Grid and Indicator components
         improvements, new hook and 10+ other changes
       </Text>
 
-      <Text className="text-[28px] text-cyan-950">4x traffic in 3 months</Text>
+      <Text className="text-lg md:text-[28px] text-cyan-950">
+        4x traffic in 3 months
+      </Text>
     </Card>
   );
 }
