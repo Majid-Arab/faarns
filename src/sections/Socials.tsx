ec50286d3d86aeaae0aea394aea523329152c87a
Majid-Arab <arabmajid12@gmail.com>
2025-04-11 22:25:10 +0500

    social responsiv

--- src/sections/Socials.tsx
+++ src/sections/Socials.tsx
@@ -1,5 +1,5 @@
 import ItalicText from "@/components/ItlaicText";
-import { Box, Group, Text, Title } from "@mantine/core";
+import { Box, Button, Group, Text } from "@mantine/core";
 import { IconArrowRight, IconArrowUpRight } from "@tabler/icons-react";
 import Image from "next/image";
 import React from "react";
@@ -7,41 +7,45 @@ import React from "react";
 const Socials = () => {
   return (
     <div className="flex justify-center px-2 py-2 bg-white">
-      <div className="flex items-center justify-between w-[1400px] text-black">
+      <div className="flex flex-col md:flex-row items-center justify-between w-[1400px] text-black">
         <Text className="uppercase font-bold text-lg md:text-xl ">
           Got <ItalicText text="MORE" size="22px" />
           <br />
           <span className="flex items-center">
             questions?
-            <IconArrowRight size={30} />
+            <IconArrowRight size={30} className="rotate-90 md:rotate-0" />
           </span>
         </Text>
-        <Box className="flex flex-col gap-[10px]">
-          <Title className="flex items-center capitalize text-center md:text-start text-3xl md:text-7xl ">
+        <Box className="flex flex-col gap-[10px] mt-5 md:mt-0">
+          <Button
+            variant="transparent"
+            c="black"
+            className="flex items-center capitalize text-center md:text-start text-4xl md:text-7xl h-full"
+          >
             Contact&nbsp;
             <ItalicText text="Us" size="70px" />
             <IconArrowUpRight size={45} className="text-purple-600" />
-          </Title>
+          </Button>
           <hr className="text-purple-600" />
           <Group justify="center" gap={45} mt={5}>
             <Image
               src="/assets/facebook.svg"
               alt="Facebook"
-              className="w- md:w-[40px] w- md:h-[40px]"
+              className="w-[30px] h-[30px] md:w-[40px] w- md:h-[40px]"
               width={500}
               height={500}
             />
             <Image
               src="/assets/instagram.svg"
               alt="Facebook"
-              className="w- md:w-[40px] w- md:h-[40px]"
+              className="w-[30px] h-[30px] md:w-[40px] w- md:h-[40px]"
               width={500}
               height={500}
             />
             <Image
               src="/assets/linkedin.svg"
               alt="Facebook"
-              className="w- md:w-[40px] w- md:h-[40px]"
+              className="w-[30px] h-[30px] md:w-[40px] w- md:h-[40px]"
               width={500}
               height={500}
             />
