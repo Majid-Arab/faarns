a9cfc2042da97248ccd0e235fdc7ce6ee68c2771
Abdul Majid <arabmajid12@gmail.com>
2025-04-11 17:26:38 +0500

    format

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -15,8 +15,13 @@ const Contact = () => {
           <div className="flex justify-start w-full">
             <Text className="max-w-[250px] md:max-w-[280px] text text-xl md:text-[25px]">
               Generating leads is the first step;{" "}
-              <span className="font-bold font-playfair italic text-[24px]">true profit</span> comes from converting
-              those leads into long-term <span className="font-bold font-playfair italic text-[24px]">client</span>
+              <span className="font-bold font-playfair italic text-[24px]">
+                true profit
+              </span>{" "}
+              comes from converting those leads into long-term{" "}
+              <span className="font-bold font-playfair italic text-[24px]">
+                client
+              </span>
             </Text>
           </div>
           <div className="flex justify-center items-center w-full">
@@ -35,9 +40,16 @@ const Contact = () => {
           </div>
           <div className="flex justify-end w-full">
             <Text className="max-w-[250px] md:max-w-[280px] md:text-xl text-xl md:text-[22px] text-center">
-              Got <span className="font-bold font-playfair italic text-[24px]">marketing</span> questions? Book a
-              free call and see why we’re the
-              <span className="font-bold font-playfair italic text-[24px]"> right fit</span> for you.
+              Got{" "}
+              <span className="font-bold font-playfair italic text-[24px]">
+                marketing
+              </span>{" "}
+              questions? Book a free call and see why we&apos;re the
+              <span className="font-bold font-playfair italic text-[24px]">
+                {" "}
+                right fit
+              </span>{" "}
+              for you.
             </Text>
           </div>
         </Group>
