9729f33dff7424c918943d8ec5e96c9181de6928
Majid-Arab <arabmajid12@gmail.com>
2025-04-11 22:08:41 +0500

    italic text styling fixes

--- src/components/ItlaicText.tsx
+++ src/components/ItlaicText.tsx
@@ -1,11 +1,17 @@
 type Prop = {
-    text: string
-}
+  text: string;
+  size?: string;
+};
 
-const ItalicText = ({ text }: Prop) => {
-    return (
-        <span className="font-bold font-playfair italic text-[40px] md:text-[52px]">{text}</span>
-    )
-}
+const ItalicText = ({ text, size = "40px" }: Prop) => {
+  return (
+    <span
+      className="font-bold font-playfair italic md:text-[52px]"
+      style={{ fontSize: size }}
+    >
+      {text}
+    </span>
+  );
+};
 
-export default ItalicText
+export default ItalicText;
