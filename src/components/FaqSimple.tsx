8a4f1444a48700a0180da7fe5f7ab38ad5b3d237
Abdul Majid <arabmajid12@gmail.com>
2025-04-11 17:25:31 +0500

    faq section created with responsvieness

--- src/components/FaqSimple.tsx
+++ src/components/FaqSimple.tsx
@@ -1,59 +1,65 @@
-import { Accordion, Container, Grid, Title } from "@mantine/core";
-// import image from "./image.svg";
-import classes from "./FaqSimple.module.css";
+import {
+  Accordion,
+  AccordionControl,
+  AccordionItem,
+  AccordionPanel,
+  Container,
+} from "@mantine/core";
+import { Icon123, IconChevronDown } from "@tabler/icons-react";
 
 const placeholder =
   "It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.";
 
+const faqData = [
+  {
+    value: "reset-password",
+    question: "How can I reset my password?",
+    answer: placeholder,
+  },
+  {
+    value: "another-account",
+    question: "Can I create more than one account?",
+    answer: placeholder,
+  },
+  {
+    value: "newsletter",
+    question: "How can I subscribe to monthly newsletter?",
+    answer: placeholder,
+  },
+  {
+    value: "credit-card",
+    question: "Do you store credit card information securely?",
+    answer: placeholder,
+  },
+];
+
 export function FaqSimple() {
   return (
-    <div className={classes.wrapper}>
-      <Container size="lg">
-        <Grid id="faq-grid" gutter={50}>
-          <Grid.Col span={{ base: 12, md: 6 }}>
-            {/* <Image src={image.src} alt="Frequently Asked Questions" /> */}
-          </Grid.Col>
-          <Grid.Col span={{ base: 12, md: 6 }}>
-            <Title order={2} ta="left" className={classes.title}>
-              Frequently Asked Questions
-            </Title>
-
-            <Accordion
-              chevronPosition="right"
-              defaultValue="reset-password"
-              variant="separated"
-            >
-              <Accordion.Item className={classes.item} value="reset-password">
-                <Accordion.Control>
-                  How can I reset my password?
-                </Accordion.Control>
-                <Accordion.Panel>{placeholder}</Accordion.Panel>
-              </Accordion.Item>
-
-              <Accordion.Item className={classes.item} value="another-account">
-                <Accordion.Control>
-                  Can I create more that one account?
-                </Accordion.Control>
-                <Accordion.Panel>{placeholder}</Accordion.Panel>
-              </Accordion.Item>
-
-              <Accordion.Item className={classes.item} value="newsletter">
-                <Accordion.Control>
-                  How can I subscribe to monthly newsletter?
-                </Accordion.Control>
-                <Accordion.Panel>{placeholder}</Accordion.Panel>
-              </Accordion.Item>
-
-              <Accordion.Item className={classes.item} value="credit-card">
-                <Accordion.Control>
-                  Do you store credit card information securely?
-                </Accordion.Control>
-                <Accordion.Panel>{placeholder}</Accordion.Panel>
-              </Accordion.Item>
-            </Accordion>
-          </Grid.Col>
-        </Grid>
-      </Container>
-    </div>
+    <Accordion
+      chevronPosition="right"
+      defaultValue="reset-password"
+      chevronSize={50}
+      className="text[#242424]"
+      chevron={<IconChevronDown style={{ width: "60%", height: "60%" }} />}
+    >
+      {faqData.map((item) => (
+        <AccordionItem
+          key={item.value}
+          value={item.value}
+          className="border-[#363434]"
+        >
+          <AccordionControl
+            // py={20}
+            px={0}
+            className="py-1 md:py-2 text-xl md:text-2xl text-white hover:bg-[#242424] rounded-md"
+          >
+            <b>{item.question}</b>
+          </AccordionControl>
+          <AccordionPanel className="text-md md:text-xl">
+            {item.answer}
+          </AccordionPanel>
+        </AccordionItem>
+      ))}
+    </Accordion>
   );
 }
