7ed4453cefa5c7e3b2291fdec3eaae7f68f11ab9
Abdul Majid <arabmajid12@gmail.com>
2025-04-19 15:40:39 +0500

    hero section spacing

--- src/components/main/Hero.tsx
+++ src/components/main/Hero.tsx
@@ -5,7 +5,7 @@ import NeonLine from "./TubeLight";
 
 const Hero = () => {
   return (
-    <Box className="flex flex-col items-center py-4">
+    <Box className="flex flex-col items-center pt-4 pb-8">
       <Box className="flex flex-col items-center justify-center gap-1">
         <Image
           src="/assets/logo.png"
