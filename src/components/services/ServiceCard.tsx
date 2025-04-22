f89c21cb150a43cd8e2ddf10e48c38b9e625e6cb
Majid-Arab <arabmajid12@gmail.com>
2025-04-22 05:10:04 +0500

    animating cards,
    scroll animation
    stack cards

--- src/components/services/ServiceCard.tsx
+++ src/components/services/ServiceCard.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { Box, Card, Text } from "@mantine/core";
 import classes from "./ServiceCard.module.css";
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "motion/react";
 
 const mockdata = [
   {
@@ -29,38 +33,54 @@ const mockdata = [
 ];
 
 export function ServiceCard() {
-  const cards = mockdata.map((service) => (
-    <Card
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start end", "start start"],
+  });
+
+  const scale = useTransform(scrollYProgress, [0, 1], [2, 1]);
+  const cards = mockdata.map((service, index) => (
+    <div
+      className="sticky h-screen"
       key={service.title}
-      p="xl"
-      shadow="lg"
-      className={classes.card}
-      radius="40"
-      component="a"
-      href="#"
+      style={{ top: `${50 + index * 40}px` }}
     >
-      <div
-        className={classes.image}
-        style={{
-          backgroundImage: `url(${service.image})`,
-        }}
-      />
-      <div className={classes.overlay} />
+      <Card
+        p="xl"
+        shadow="lg"
+        className={classes.card}
+        radius="40"
+        component="a"
+        href="#"
+      >
+        <motion.div
+          className={classes.image}
+          style={{
+            scale,
+            backgroundImage: `url(${service.image})`,
+          }}
+        />
+        <div className={classes.overlay} />
 
-      <div className={classes.content}>
-        <div>
-          <Text size="lg" className={classes.title} fw={800}>
-            {service.title}
-          </Text>
+        <div className={classes.content}>
+          <div>
+            <Text size="lg" className={classes.title} fw={800}>
+              {service.title}
+            </Text>
 
-          <Text size="sm" className={classes.description}>
-            {service.description}
-          </Text>
+            <Text size="sm" className={classes.description}>
+              {service.description}
+            </Text>
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </div>
   ));
+
   return (
-    <Box className="flex flex-col gap-5 mx-2 lg:max-w-60 py-5">{cards}</Box>
+    <Box className="flex flex-col gap-5 mx-2 lg:max-w-60 py-5 relative">
+      {cards}
+    </Box>
   );
 }
