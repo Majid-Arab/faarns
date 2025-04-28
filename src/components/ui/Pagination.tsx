9717cbc20f4cbdc29e9c568f02da4ea9f49bdc3e
Majid-Arab <arabmajid12@gmail.com>
2025-04-28 05:13:29 +0500

    pagination responsive setup

--- src/components/ui/Pagination.tsx
+++ src/components/ui/Pagination.tsx
@@ -1,9 +1,18 @@
 import { Pagination } from "@mantine/core";
 import React from "react";
+import { useMediaQuery } from "@mantine/hooks";
 import classes from "./Pagination.module.css";
 
 const PaginationBar = () => {
-  return <Pagination total={10} classNames={classes} />;
+  const isMobile = useMediaQuery("(max-width: 768px)");
+  return (
+    <Pagination
+      total={10}
+      classNames={classes}
+      gap={isMobile ? 2 : 8}
+      size={isMobile ? "sm" : "md"}
+    />
+  );
 };
 
 export default PaginationBar;
